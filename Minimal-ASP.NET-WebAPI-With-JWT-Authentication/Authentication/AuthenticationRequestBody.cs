7d15efad1002e5ea008370183b526a890fc793c8
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-10 16:58:01 -0300

    Add AuthenticationController

---
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Authentication/AuthenticationRequestBody.cs
@@ -0,0 +1,8 @@
+﻿namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Authentication
+{
+    public class AuthenticationRequestBody
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Authentication;
+using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Models;
+using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Repositories;
+
+namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Controllers
+{
+    [ApiController]
+    [Route("api/authentication")]
+    public class AuthenticationController : Controller
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IUserRepository _userRepository;
+
+        public AuthenticationController(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+        {
+            var user = ValidateUserCredentials(authenticationRequestBody);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(_jwtTokenGenerator.GenerateToken(user));
+
+        }
+
+        private User ValidateUserCredentials(AuthenticationRequestBody authenticationRequestBody)
+        {
+            var user = _userRepository.Get(authenticationRequestBody.Username, authenticationRequestBody.Password).Result;
+            if (user == null)
+            {
+                return null;
+            }
+
+            return user;
+
+        }
+    }
+}
