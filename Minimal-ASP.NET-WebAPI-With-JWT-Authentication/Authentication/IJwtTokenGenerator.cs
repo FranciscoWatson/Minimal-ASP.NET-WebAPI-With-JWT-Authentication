3c6833f68dd6f1398cc116bad36db6454657c226
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-10 15:19:13 -0300

    Add IJwtTokenGenerator

---
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Models;
+
+namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Authentication
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(User user);
+    }
+}
