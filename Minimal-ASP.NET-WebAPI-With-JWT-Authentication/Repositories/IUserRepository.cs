91be29aabee4867ce0ea6d6663c0c367c8fac33d
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-10 16:56:24 -0300

    Add User Repository

---
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Models;
+
+namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> Get(string username, string password);
+    }
+}
---
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Models;
+
+namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly List<User> _users = new List<User>
+        {
+            new User { UserId = 1, FirstName = "John", LastName = "Doe", Username = "john.doe", Password = "password" },
+            new User { UserId = 2, FirstName = "Jane", LastName = "Doe", Username = "jane.doe", Password = "password" }
+        };
+
+        public Task<User> Get(string username, string password)
+        {
+            return Task.FromResult(_users.FirstOrDefault(u => u.Username == username && u.Password == password));
+        }
+    }
+}
