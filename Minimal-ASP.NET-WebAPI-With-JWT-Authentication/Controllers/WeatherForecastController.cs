21bf7dfd4b282b90b854e5f63fc5414a6b2f6c15
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-10 17:46:30 -0300

    Add Authorize to WeatherForeCastController

--- Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Controllers/WeatherForecastController.cs
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
