8e9d12fad2d9b9ce0babe0de05e442778d390891
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-10 17:51:57 -0300

    Add Dependencies Injection in Program.cs

--- Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Program.cs
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Program.cs
@@ -1,8 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Authentication;
+using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var jwtConfig = builder.Configuration.GetSection("JwtAuthentication").Get<JwtAuthenticationConfig>();
+
+builder.Services.AddSingleton(jwtConfig);
+
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddControllers();
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtConfig.Issuer,
+            ValidAudience = jwtConfig.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(jwtConfig.SecretKey))
+    };
+    });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
