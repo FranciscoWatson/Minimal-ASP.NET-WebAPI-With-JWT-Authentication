42bb4fe54f908aa5de58c4f30c03af2a16334ec0
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-11 10:30:17 -0300

    Add more information returned when authenticating an User

--- Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Controllers/AuthenticationController.cs
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Controllers/AuthenticationController.cs
@@ -11,15 +11,17 @@ namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Controllers
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
         private readonly IUserRepository _userRepository;
+        private readonly JwtAuthenticationConfig _jwtAuthenticationConfig;
 
-        public AuthenticationController(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+        public AuthenticationController(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository, JwtAuthenticationConfig jwtAuthenticationConfig)
         {
             _jwtTokenGenerator = jwtTokenGenerator;
             _userRepository = userRepository;
+            _jwtAuthenticationConfig = jwtAuthenticationConfig;
         }
 
         [HttpPost("authenticate")]
-        public ActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+        public ActionResult<object> Authenticate(AuthenticationRequestBody authenticationRequestBody)
         {
             var user = ValidateUserCredentials(authenticationRequestBody);
 
@@ -28,7 +30,21 @@ namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Controllers
                 return Unauthorized();
             }
 
-            return Ok(_jwtTokenGenerator.GenerateToken(user));
+            var token = _jwtTokenGenerator.GenerateToken(user);
+            var expiration = DateTime.UtcNow.AddMinutes(_jwtAuthenticationConfig.TokenExpiryInMinutes);
+
+            return Ok(new
+            {
+                access_token = token,
+                token_type = "Bearer",
+                expires_in = expiration,
+                user_info = new
+                {
+                    user_id = user.UserId,
+                    first_name = user.FirstName,
+                    last_name = user.LastName
+                }
+            });
 
         }
 
