fc5f76e39414fe9c9d8538073e6ad346ebc4d215
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-11 10:50:58 -0300

    Add Hashing to Users passwords

--- Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Repositories/UserRepository.cs
+++ Minimal-ASP.NET-WebAPI-With-JWT-Authentication/Repositories/UserRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Repositories
 {
@@ -6,13 +8,22 @@ namespace Minimal_ASP.NET_WebAPI_With_JWT_Authentication.Repositories
     {
         private readonly List<User> _users = new List<User>
         {
-            new User { UserId = 1, FirstName = "John", LastName = "Doe", Username = "john.doe", Password = "password" },
-            new User { UserId = 2, FirstName = "Jane", LastName = "Doe", Username = "jane.doe", Password = "password" }
+            new User { UserId = 1, FirstName = "John", LastName = "Doe", Username = "john.doe", Password = HashPassword("password") },
+            new User { UserId = 2, FirstName = "Jane", LastName = "Doe", Username = "jane.doe", Password = HashPassword("password") }
         };
 
         public async Task<User> Get(string username, string password)
         {
-            return await Task.FromResult(_users.FirstOrDefault(u => u.Username == username && u.Password == password));
+            var hashedPassword = HashPassword(password);
+            return await Task.FromResult(_users.FirstOrDefault(u => u.Username == username && u.Password == hashedPassword));
         }
+
+        private static string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return BitConverter.ToString(hashedBytes).Replace("-", "").ToLowerInvariant();
+        }
+
     }
 }
